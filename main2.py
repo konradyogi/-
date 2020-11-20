5c6c87c64012e526f565f9043df71f31c9a8cb39
Konrad J <60574355+konradyogi@users.noreply.github.com>
2020-11-20 09:46:25 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -51,9 +51,29 @@ def negative(image):
 
 def generate_cehssboard(size):
     pass
-def generate_diagonal(size):
-    pass
 
+##############################################
+
+def generate_diagonal(size):
+    image = {}
+    image["type"] = "P1"
+    image["size"] = [size, size]
+    image["comment"] = ''
+    image["pixels"] = []
+
+for row in range(size):
+        line = []
+        for col in range(size):
+            if row == col:
+                line.append(1)
+            else:
+                line.append(0)
+        image["pixels"].append(line)
+
+
+return image
+
+##############################################
 
 def mirror_y(image):
     pass
